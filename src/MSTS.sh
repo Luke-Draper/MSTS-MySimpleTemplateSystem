6a35c2c70f60bd592b8ffbcd725f495af69af5de
Luke-Draper <lukepdraper@gmail.com>
2018-11-10 15:33:33 -0600

    Added Letterboxing

--- src/MSTS.sh
+++ src/MSTS.sh
@@ -3,24 +3,29 @@
 N=$'\n'
 T=$'\t'
 
+HR='|:| -+- - - - - - - - - - - - - - - -+- |:|'
+
+echo ' _________________________________________ '
+echo '|:- = - = - = - = - = = - = - = - = - = -:|'
+echo '|:| -+- - - - - - - - - - - - - - - -+- |:|'
+echo '|:| |:-  My Simple Template System  -:| |:|'
+echo '|:| |:-    Made By : Luke Draper    -:| |:|'
+echo '|:| -+- - - - - - - - - - - - - - - -+- |:|'
+echo '|:- = - = - = - = - = = - = - = - = - = -:|'
+echo '|:| -+- - - - - - - - - - - - - - - -+- |:|'
+echo '|:| -:- This is a helper script to  -:- |:|'
+echo '|:| -:- parse a JSON file using jq  -:- |:|'
+echo '|:| -:-    and pass the variables   -:- |:|'
+echo '|:| -:-  into further scripts then  -:- |:|'
+echo '|:| -:- write the output to a file  -:- |:|'
+echo '|:| -+- - - - - - - - - - - - - - - -+- |:|'
+echo '|:- = - = - = - = - = = - = - = - = - = -:|'
+echo '|:| -+- - - - - - - - - - - - - - - -+- |:|'
+
 SOURCE_JSON_FILE=$1
 
 SOURCE_JSON="$(cat ${SOURCE_JSON_FILE} | sed 's/\r$//')"
 
-# Test for required fields in each JSON object as listed below
-# {
-# "templateDirectory": Path to the file folder holding the templates
-# "tasks": Array holding objects representing a template operation in the form
-# 	[
-# 		{
-# 		"targets": Array holding objects representing a set of templates to run and where to put the output in the form
-# 			[
-# 				{
-# 					"templates": Array holding an ordered list of template base names (no .msts.sh extension) to be run to produce the output []
-# 					"destination": The file to write the template output to
-# 				}
-# 			]
-# 			"variables": The JSON object passed into the template runs as MSTS_VARS
 VALID_INPUT="$( jq -n --argjson data "$SOURCE_JSON" '$data |
 		[has("templateDirectory"),has("tasks"), (.tasks |
 		map(has("targets"),has("variables"),(.targets |
@@ -28,12 +33,12 @@ VALID_INPUT="$( jq -n --argjson data "$SOURCE_JSON" '$data |
 		flatten(100) | all')"
 
 if [ "$VALID_INPUT" = "true" ]; then
-	echo "Valid JSON Input"
+	echo "|:| Valid JSON Input  - - - - - - - -+- |:|"
 else
-	echo 'Invalid JSON Input'
-	echo ''
-	echo 'JSON is required to this format exactly. Double check spelling :'
-	echo ''
+	echo '|:| Invalid JSON Input'
+	echo '|:|'
+	echo '|:| JSON is required to this format exactly. Double check spelling |:|'
+	echo '|:|'
 	echo '|:| '
 	echo '|:| {'
 	echo '|:| "templateDirectory": // Path to the file folder holding the templates'
@@ -45,17 +50,18 @@ else
 	echo '|:|         {'
 	echo '|:|           "templates": // Array holding an ordered list of template base names (no .msts.sh extension) to be run to produce the output []'
 	echo '|:|           "destination": // The file to write the template output to'
-	echo '|:|         }, ...'
+	echo '|:|         }, ... // More templates and file destinations using these variables as needed'
 	echo '|:|       ]'
 	echo '|:|       "variables": // The JSON object passed into the template runs as MSTS_VARS'
-	echo '|:|     }, ...'
+	echo '|:|     }, ... // More variable sets as needed'
 	echo '|:|   ]'
 	echo '|:| }'
-	echo '|:| '
-	echo ''
+	echo '|:|'
+	echo '|:|'
 	exit 1
 fi
 
+echo "$HR"
 
 TEMPLATE_DIRECTORY="$( jq -n --argjson data "$SOURCE_JSON" '$data.templateDirectory')"
 TEMPLATE_DIRECTORY="${TEMPLATE_DIRECTORY%\"}"
@@ -73,7 +79,7 @@ function setupTemplate() {
 	TEMPLATE_NAMES[$i]=$filename
 	source ${TEMPLATE_PATHS[$i]}
 	TEMPLATE_FUNCTIONS[$i]=$(declare -f $1)
-	echo "Template Loaded : $filename"
+	echo "|:| Template Loaded - - - - - - - - -+- |:| $filename"
 }
 
 for filepath in ~/${TEMPLATE_DIRECTORY}/*.msts.sh; do
@@ -86,6 +92,8 @@ for i in ${!TEMPLATE_PATHS[@]}; do
 	setupTemplate
 done
 
+echo "$HR"
+
 runTemplate() {
 	local completeFlag=""
 	for i in ${!TEMPLATE_NAMES[@]}; do
@@ -97,11 +105,29 @@ runTemplate() {
 		fi
 	done
 	if [ -z "$completeFlag" ]; then
-		echo "Unable to locate template    : $1"
+		echo "|:| Unable to locate template - - - --- |:| $1"
 	else
-		echo "Succesfully located template : $1"
+		echo "|:| Succesfully located template  - -+- |:| $1"
 	fi
 }
+
+runInternalTemplate() {
+	local completeFlag=""
+	for i in ${!TEMPLATE_NAMES[@]}; do
+		if [ "${TEMPLATE_NAMES[$i]}" = "$1" ]; then
+			completeFlag="true"
+			eval "${TEMPLATE_FUNCTIONS[$i]}"
+			echo "$(msts)"
+		fi
+	done
+	if [ -z "$completeFlag" ]; then
+		echo "|:| Unable to locate internal template  - --- |:| $1"
+	else
+		echo "|:| Succesfully located internal template -+- |:| $1"
+	fi
+}
+
+
 
 runTemplate 'test2'
 
